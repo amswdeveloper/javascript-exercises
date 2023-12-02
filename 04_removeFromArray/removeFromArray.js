c3162fa7bd2d0569ff3f6040e3c0b220dbb292fa
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2023-12-02 17:30:01 +0300

    Removes consecutive  element from array upto the number of argument provided

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...element) {
 
+const newArray = [];
+
+for(let i = 0; i < arr.length; i++) {
+
+    if(element[i]){
+        continue;
+    } else {
+        newArray.push(arr[i]);
+    }
+}
+
+console.log(newArray);
+// return newArray;
 };
+removeFromArray([3, 4, 5, 6, 1, 8, 0], 3, 4, 5, 8);
 
 // Do not edit below this line
 module.exports = removeFromArray;
