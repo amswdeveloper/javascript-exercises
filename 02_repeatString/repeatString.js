4e1cae86a7adbaa18043f55df854fb1961000d6a
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2023-12-02 11:12:55 +0300

    feat: Set up local environment and complete initial JavaScript exercises
    - Cloned the JavaScript exercises repository
    - Installed Jest for testing
    - Reviewed and followed the setup instructions in the repository's README
    - Completed exercises:
      - 01_helloWorld (ensuring proper environment setup)
      - Implement repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let test = "";
+    if(num < 0) {
+        return "ERROR";
+    }
+  for(let i = 1; i <= num; i++) {
+    test += string
+  }
+  return test;
 };
 
 // Do not edit below this line
