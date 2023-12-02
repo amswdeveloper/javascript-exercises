3fd574c85e8716fb246a5f7ffe50301cd6119c2a
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2023-12-02 13:44:49 +0300

    feat: Implement reverseString function
    - Added a new function named reverseString to address the requirements of Exercise 03
    - The reverseString function takes a string as input and returns the reversed version of the string
    - Conducted thorough testing to ensure the function handles various cases correctly
    - Updated the README with information on using the reverseString function
    - Commit includes new code and tests for Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    const splitString = inputString.split("");
 
+  let reversedString = "";
+  for(let i = splitString.length-1; i >= 0; i--) {
+    reversedString += splitString[i];
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
