a69e106058cb8f868f12f90efb6854b343214a17
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2023-12-03 10:33:16 +0300

    feat: Implement temperature conversion functions
    - Implemented two functions, fahrenheitToCelsius and celsiusToFahrenheit, to meet the requirements of Exercise 07
    - The fahrenheitToCelsius function converts temperatures from Fahrenheit to Celsius using the formula (°F - 32) * 5/9
    - The celsiusToFahrenheit function converts temperatures from Celsius to Fahrenheit using the formula (°C * 9/5) + 32
    - Conducted thorough testing with various temperature values to ensure accurate conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let fahrenheitToCelsius = (temp - 32) * (5 / 9);
+  fahrenheitToCelsius= fahrenheitToCelsius.toFixed(1);
+
+  return Number(fahrenheitToCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let celsiusToFahrenheit = temp * (9 / 5) + 32;
+  celsiusToFahrenheit = celsiusToFahrenheit.toFixed(1);
+
+  return Number(celsiusToFahrenheit);
 };
 
 // Do not edit below this line
