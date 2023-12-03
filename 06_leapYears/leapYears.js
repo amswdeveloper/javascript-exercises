b5b73414b14f8751bb4775f938a7636369e29c56
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2023-12-03 09:35:03 +0300

    feat: Implement leapYears function
    - Implemented a new function named leapYears to fulfill the requirements of Exercise 06
    - The leapYears function determines whether a given year is a leap year based on specified rules
    - Utilized conditional statements to check leap year conditions: divisible by 4 but not by 100 unless also divisible by 400
    - Conducted thorough testing to validate the accuracy of the leapYears function for various years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if(year % 4 === 0){
+        if(year % 100 !== 0) {
+            return true;
+        } else {
+            if(year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
+
+    } else {
+        return false;
+    }
 
 };
+
+console.log(leapYears(1600));
 
 // Do not edit below this line
 module.exports = leapYears;
