426d8a9ca33279eb601e67f274d7cf97fcb615f4
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2023-12-03 09:08:24 +0300

    feat: Implement sumAll function
    - Created a new function named sumAll to meet the requirements of Exercise 05
    - The sumAll function takes two integers and returns the sum of all numbers between (and including) the given integers
    - Utilized a loop to iterate through the range of numbers and accumulate the sum
    - Conducted thorough testing with various input scenarios to ensure the correctness of the sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
 
+  if( (typeof firstNum === "number") && (typeof secondNum === "number")) {
+
+    if(firstNum < 0 || secondNum < 0) {
+        console.log(firstNum, secondNum);
+        return "ERROR";
+    } else if(firstNum < secondNum) {
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+
+    } else if(secondNum < firstNum) {
+        for (let i = secondNum; i <= firstNum; i++) {
+            sum += i;
+        }
+
+    }
+
+  } else {
+    return "ERROR";
+  }   
+
+    return sum;
 };
 
 // Do not edit below this line
